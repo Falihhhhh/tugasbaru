26d767e915ef135e9cdc26145e2aeae4d0a120cd
falihhhhh <falihmuhammad199@gmail.com>
2022-01-26 23:54:01 -0800

    tugasbaru

---
+++ tugasbaru2.js
@@ -0,0 +1,11 @@
+function LessThan(number1,number2) {
+    if (number1+number2 <= 100) {
+      console.log("lessthan 100");
+    } else {
+    console.log("Morethan 100")}
+  }
+  
+  LessThan(22,15);
+  LessThan(83,34);
+  LessThan(3,77);
+  
